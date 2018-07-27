acf15aed0702b458afe851d46a58350b129f0435
johnpark <johnpark@ebay.com>
2018-07-27 14:18:57 +0900

    end2 709

--- lc709.py
+++ lc709.py
@@ -1,7 +1,10 @@
 def solve(str):
-    return str.lower()
+    res = ""
+    for c in str:
+        res += chr(ord(c)+32) if 65 <= ord(c) <= 90 else c
+    return res
 
 
-testCases = ['ab', 'Ab', 'aB']
-res = list(map(solve, testCases))
-print(res)
+testCases = ['ab', 'Ab', 'aB', 'c1', 'C1']
+result = list(map(solve, testCases))
+print(result)
