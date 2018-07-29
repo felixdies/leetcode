f84f315900239106d8fe836b2905289949e4d838
felixdies <felixdies@daum.net>
2018-07-29 21:55:38 +0900

    771 solve

--- lc771.py
+++ lc771.py
@@ -1,14 +1,7 @@
 def solve(J, S):
     res = 0
-    j = 0
-    for s in range(len(S)):
-        if S[s] == J[j]: j+=1
-        elif S[s] == J[0]: j=1
-        else: j=0
-
-        if j == len(J):
-            j=0
-            res+=1
+    for s in S:
+        if s in J: res+=1
     return res
 
 
