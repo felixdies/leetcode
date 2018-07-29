6b5b7cd99717865e7b430cbbb09ca5c71e501376
felixdies <felixdies@daum.net>
2018-07-29 22:05:59 +0900

    TestCase.py

--- TestCases.py
+++ TestCases.py
@@ -1,12 +1,16 @@
+""" 매개변수 1개 """
 def solve(arg): return 1
-# 매개변수 1개
+
+
 arr1 = [1,2,3]
 result = list(map(solve, arr1))
 print(result)
 
 
+""" 매개변수 2개 """
 def solve(arg1, arg2): return 2
-# 매개변수 2개
+
+
 arr1 = [1,2,3]
 arr2 = [4,5,6]
 result = list(map(solve, arr1, arr2))
